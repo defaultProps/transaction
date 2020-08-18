70b8b9e76d8fb7557b4441da20aaa901ce9d3f8e
1748416084@qq.com <1748416084@qq.com>
2020-08-18 15:45:05 +0800

    更新demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NProgress from 'nprogress'
-
 import routes from './route'
 
 Vue.use(Router)
@@ -9,7 +8,7 @@ Vue.use(Router)
 let router = new Router({
   routes,
   linkActiveClass: 'active',
-  mode: 'history'
+  mode: 'hash'
 })
 
 router.beforeEach((to, from, next) => {
